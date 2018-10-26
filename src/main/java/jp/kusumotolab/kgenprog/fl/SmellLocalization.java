37441cac8678612ac878582a7122010129bc0998
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2018-10-26 17:45:54 +0900

    Implement calculation of smell #5
    Calculate complexity of code fragment as smell

--- src/main/java/jp/kusumotolab/kgenprog/fl/SmellLocalization.java
+++ src/main/java/jp/kusumotolab/kgenprog/fl/SmellLocalization.java
@@ -3,9 +3,17 @@ package jp.kusumotolab.kgenprog.fl;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import jp.kusumotolab.kgenprog.project.ASTLocation;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ConditionalExpression;
+import org.eclipse.jdt.core.dom.DoStatement;
+import org.eclipse.jdt.core.dom.EnhancedForStatement;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.WhileStatement;
 import jp.kusumotolab.kgenprog.project.GeneratedAST;
 import jp.kusumotolab.kgenprog.project.GeneratedSourceCode;
+import jp.kusumotolab.kgenprog.project.jdt.JDTASTLocation;
 import jp.kusumotolab.kgenprog.project.test.TestResults;
 
 public class SmellLocalization implements FaultLocalization {
@@ -20,24 +28,79 @@ public class SmellLocalization implements FaultLocalization {
       final int lastLineNumber = countLines(code);
 
       for (int line = 1; line <= lastLineNumber; ++line) {
-        final List<ASTLocation> locations = ast.inferLocations(line);
+        // todo: remove down cast
+        final List<JDTASTLocation> locations = ast.inferLocations(line);
 
         if (locations.isEmpty()) {
           continue;
         }
 
-        suspiciousnesses.addAll(locations.stream()
-            .map(location -> new Suspiciousness(location, 1.0)) // todo: calculate smell
-            .collect(Collectors.toList()));
+        for (JDTASTLocation location : locations) {
+          final ComplexityVisitor visitor = new ComplexityVisitor();
+          location.node.accept(visitor);
+          suspiciousnesses.add(new Suspiciousness(location, visitor.getComplexity()));
+        }
       }
     }
 
-    // todo: remove duplicated elements
-    return suspiciousnesses;
+    return suspiciousnesses.stream()
+        .distinct()
+        .collect(Collectors.toList());
   }
 
   private int countLines(final String text) {
     final String[] lines = text.split("\r\n|\r|\n");
     return lines.length;
+  }
+
+  static private class ComplexityVisitor extends ASTVisitor {
+
+    private int complexity = 0;
+
+    public int getComplexity() {
+      return complexity;
+    }
+
+    @Override
+    public boolean visit(MethodDeclaration node) {
+      ++complexity;
+      return super.visit(node);
+    }
+
+    @Override
+    public boolean visit(ConditionalExpression node) {
+      ++complexity;
+      return super.visit(node);
+    }
+
+    @Override
+    public boolean visit(EnhancedForStatement node) {
+      ++complexity;
+      return super.visit(node);
+    }
+
+    @Override
+    public boolean visit(ForStatement node) {
+      ++complexity;
+      return super.visit(node);
+    }
+
+    @Override
+    public boolean visit(DoStatement node) {
+      ++complexity;
+      return super.visit(node);
+    }
+
+    @Override
+    public boolean visit(WhileStatement node) {
+      ++complexity;
+      return super.visit(node);
+    }
+
+    @Override
+    public boolean visit(IfStatement node) {
+      ++complexity;
+      return super.visit(node);
+    }
   }
 }
