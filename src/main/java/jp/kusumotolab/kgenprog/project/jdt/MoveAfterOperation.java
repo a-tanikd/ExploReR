f68f3e35edab5f40180d53249ad09dc3ea422142
a-tanikd <8575113+a-tanikd@users.noreply.github.com>
2019-01-20 16:41:15 +0900

    Add MoveAfterOperation #30

---
+++ src/main/java/jp/kusumotolab/kgenprog/project/jdt/MoveAfterOperation.java
@@ -0,0 +1,40 @@
+package jp.kusumotolab.kgenprog.project.jdt;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
+import jp.kusumotolab.kgenprog.project.SourcePath;
+
+public class MoveAfterOperation extends JDTOperation {
+
+  private final ASTNode astNode;
+
+  public MoveAfterOperation(final ASTNode astNode) {
+    this.astNode = astNode;
+  }
+
+  @Override
+  protected <T extends SourcePath> void applyToASTRewrite(final GeneratedJDTAST<T> ast,
+      final JDTASTLocation location, final ASTRewrite astRewrite) {
+
+    // astNode の Location 作成
+    final JDTASTLocation location2 = new JDTASTLocation(location.getSourcePath(), astNode, null);
+
+    final ASTNode target1 = location.locate(ast.getRoot());
+    final ASTNode target2 = location2.locate(ast.getRoot());
+
+    final ASTNode copiedTarget2 = astRewrite.createCopyTarget(astNode);
+
+    final ListRewrite listRewrite = astRewrite.getListRewrite(target1.getParent(),
+        (ChildListPropertyDescriptor) target1.getLocationInParent());
+
+    listRewrite.insertAfter(copiedTarget2, target1, null);
+    astRewrite.remove(target2, null);
+  }
+
+  @Override
+  public String getName() {
+    return "move after";
+  }
+}
